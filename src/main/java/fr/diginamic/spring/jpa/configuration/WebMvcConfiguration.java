00460fa51869a24bc4adbf92926829511cbfabc8
AntoninB4440 <barceloantonin@gmail.com>
2021-04-02 15:50:01 +0200

    update dependencies

--- src/main/java/fr/diginamic/spring/jpa/configuration/WebMvcConfiguration.java
+++ src/main/java/fr/diginamic/spring/jpa/configuration/WebMvcConfiguration.java
@@ -1,5 +1,6 @@
 package fr.diginamic.spring.jpa.configuration;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@ComponentScan("fr.diginamic.spring.jpa")
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
